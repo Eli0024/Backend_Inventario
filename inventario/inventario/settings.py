8ab9918614189f5351687a94dd124bf6ddeef08c
Eli0024 <eliana199972@gmail.com>
2024-10-30 16:19:47 -0500

    Arreglado El Error

--- inventario/inventario/settings.py
+++ inventario/inventario/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,6 +85,9 @@ DATABASES = {
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
+        # 'OPTIONS': {
+        #     'ssl': False,  
+        # },
     }
 }
 
